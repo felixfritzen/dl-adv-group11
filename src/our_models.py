e4ec1f15e81064c3087af977398a23d9b03b2b28
astridfrykman <felixfritzen00@gmail.com>
2024-12-01 13:02:15 +0000

    waterbirds teacher

--- src/our_models.py
+++ src/our_models.py
@@ -1,7 +1,14 @@
 from safetensors.torch import load_file
 import torch
 import torchvision.models as models
+from fixup_resnet import xfixup_resnet50 
 
+
+MODEL_PATH = "/home/shared_project/dl-adv-group11/models/weights/"
+FIGURE_PATH = "/home/shared_project/dl-adv-group11/figs/"
+
+MODEL_PATH +="waterbirds/"
+FIGURE_PATH +="waterbirds/"
 
 def pcam_teacher():
     checkpoint_path = "/home/shared_project/dl-adv-group11/models/teachers/pcam/model.safetensors"
@@ -22,6 +29,13 @@ def pcam_student():
         out_features=2,  # Binary
         bias=True
     )
+    return model
+
+def waterbirds_teacher():
+    model = xfixup_resnet50(num_classes=2)  # Ensure this matches the fine-tuned model structure
+    model.fc = torch.nn.Linear(model.fc.in_features, 2)  # Ensure the final layer is binary classification
+    fine_tuned_weights_path =MODEL_PATH +"resnet50_augmented_fc_200epoker.pth"
+    model.load_state_dict(torch.load(fine_tuned_weights_path))
     return model
 
 def waterbirds_student():
